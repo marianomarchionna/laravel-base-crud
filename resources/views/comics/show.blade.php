39657035f9e502d8f8264c7a89c580a14c449f97
Mariano <marianomarchionna8@gmail.com>
2021-11-02 17:02:08 +0100

    Stampa tutti i dettagli

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <p>{{ $comic['description'] }}</p>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="mt-4 mb-4">Dettaglio {{ $comic['title'] }}</h1>
+            <img src="{{ $comic['thumb'] }}" alt="Immagine Fumetto">
+            <ul>
+                <li>Tipo: {{ $comic['type'] }};</li>
+                <li>Descrizione: {{ $comic['description'] }};</li>
+                <li>Prezzo: {{ $comic['price'] }} €;</li>
+                <li>Serie: {{ $comic['series'] }};</li>
+                <li>Data di vendita: {{ $comic['sale_date'] }};</li>
+                <li>Tipo: {{ $comic['type'] }};</li>
+            </ul>
+        </div>
     </div>
+</div>
 @endsection
