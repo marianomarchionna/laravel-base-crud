f6d8847d37dfce3fea0ba4cf93db5eef010a9c34
Mariano <marianomarchionna8@gmail.com>
2021-11-02 16:28:24 +0100

    Visualizzazione con bootstrap in index.blade e nav-bar

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,6 +2,33 @@
 
 @section('content')
     <div class="container">
-        @dump($comics)
+        <div class="row">
+            <div class="col-12">
+                <h1 class="mt-4 mb-4">Tutti i fumetti</h1>
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Cooking Time</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($comics as $comic)
+                            <tr>
+                                <th scope="row">{{ $comic['id'] }}</th>
+                                <td>{{ $comic['title'] }}</td>
+                                <td>{{ $comic['price'] }}</td>
+                                <td>{{ $comic['series'] }}</td>
+                                <td>{{ $comic['sale_date'] }}</td>
+                                
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="{{ route('pagina-home') }}">I miei fumetti</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="{{ route('pagina-home') }}">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('comics.create')}}">Inserisci un fumetto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('comics.index') }}">Lista dei fumetti</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
